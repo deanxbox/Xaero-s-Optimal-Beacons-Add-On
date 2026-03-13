951caed49c1027ec881c3fada5e144757b090691
Dean Mahmood <dean.mahmood05@gmail.com>
2026-03-13 07:07:42 +0000

    Initial commit, start of the mod

---
+++ src/client/java/deanxbox/xaeros_beacon_addon/XaerosOptimalBeaconsAddOnClient.java
@@ -0,0 +1,10 @@
+package deanxbox.xaeros_beacon_addon;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class XaerosOptimalBeaconsAddOnClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
